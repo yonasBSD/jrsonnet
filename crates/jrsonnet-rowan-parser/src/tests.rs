2223f9ab687dfe117fc2264cba600dcb03f2e7b8
Yaroslav Bolyukin <iam@lach.pw>
2026-05-07 23:10:06 +0200

    fix: incorrect doccomment parsing

--- crates/jrsonnet-rowan-parser/src/tests.rs
+++ crates/jrsonnet-rowan-parser/src/tests.rs
@@ -241,6 +241,25 @@ mk_test!(
 	for_obj_spec_value_destruct => r#"
 		{ [k]: a + b for [k]: [a, b] in obj }
 	"#
+
+	multi_line_comment_doc => r#"
+		{ a:: 1, /** doc **/ b:: 2 }
+	"#
+	multi_line_comment_empty => r#"
+		/**/ 1
+	"#
+	multi_line_comment_triple_star => r#"
+		/***/ 2
+	"#
+	multi_line_comment_inner_star => r#"
+		/* * */ 3
+	"#
+	multi_line_comment_too_short => r#"
+		/*/
+	"#
+	multi_line_comment_unterminated => r#"
+		/** unterminated
+	"#
 );
 
 #[test]
